0d9bf9248fa28af474caca731694bf02979e2f63
Rajib-Ranjan-Kumar <jaiswalrajib98192@gmail.com>
2025-04-18 20:26:53 +0530

    Connecting To FirebaseAuth and FirebaseStore

--- app/src/main/java/com/example/livechat99/di/LiveChatModule.kt
+++ app/src/main/java/com/example/livechat99/di/LiveChatModule.kt
@@ -1,6 +1,11 @@
 package com.example.livechat99.di
 
 import androidx.lifecycle.ViewModel
+import com.google.firebase.Firebase
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.auth
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.firestore
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -10,5 +15,8 @@ import dagger.hilt.android.components.ViewModelComponent
 @InstallIn(ViewModelComponent::class)
 class LiveChatModule {
     @Provides
-    fun getFirebaseAuth()
+    fun getFirebaseAuth():FirebaseAuth= Firebase.auth
+
+    @Provides
+    fun getFireBaseFireStore():FirebaseFirestore=Firebase.firestore
 }
--- app/src/main/java/com/example/livechat99/viewmodel/LiveChatViewModel.kt
+++ app/src/main/java/com/example/livechat99/viewmodel/LiveChatViewModel.kt
@@ -1,10 +1,15 @@
 package com.example.livechat99.viewmodel
 
 import androidx.lifecycle.ViewModel
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
 @HiltViewModel
-class LiveChatViewModel @Inject constructor():ViewModel() {
+class LiveChatViewModel @Inject constructor(
+    private var auth: FirebaseAuth,
+    private var db:FirebaseFirestore
+):ViewModel() {
 
 }
